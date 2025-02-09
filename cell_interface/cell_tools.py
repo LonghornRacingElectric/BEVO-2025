a648f8b5c5f6f92c4b6985719178ae3cb1cd880b
Anish-Y <55471849+Anish-Y@users.noreply.github.com>
2025-02-09 15:12:11 -0600

    cell tools

---
+++ cell_interface/cell_tools.py
@@ -0,0 +1,44 @@
+import serial
+import RPi.GPIO as GPIO
+import time
+
+# Serial Defines
+PORT = '/dev/ttyUSB2'
+BAUDRATE = 115200
+
+# GPIO setup
+FULL_CARD_POWER_OFF = 16
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(FULL_CARD_POWER_OFF, GPIO.OUT)
+
+
+def shutdown_module():
+    try:
+        with serial.Serial(port=PORT, baudrate=BAUDRATE, timeout=1) as s:
+            s.write(b'AT+CFUN=0\r\n') #send byte code for AT+CFUN=0 (shutdown)
+            response = s.readline()
+            if response == "OK":
+                GPIO.output(FULL_CARD_POWER_OFF, GPIO.LOW)
+                time.sleep(1)
+            else:
+                print("Module Shutdown Failed")
+    except serial.SerialException as e:
+        print(f"SERIAL SHUTDOWN ERROR: {e}") 
+    except Exception as e:
+        print(f"UNEXPECTED SHUTDOWN ERROR: {e}")
+        
+def power_on_module():
+    try:
+        GPIO.output(FULL_CARD_POWER_OFF, GPIO.HIGH)
+    except Exception as e:
+        print(f"UNEXPECTED POWER_ON ERROR: {e}")
+     
+def main():
+    try:
+        power_on_module()
+    finally:
+        shutdown_module()
+        GPIO.cleanup()
+        
+if __name__=='__main__':
+    main()
