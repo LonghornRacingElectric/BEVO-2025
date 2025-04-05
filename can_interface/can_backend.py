1b8541f621ef72ee146b5aed97801d7bc592666d
Anish-Y <55471849+Anish-Y@users.noreply.github.com>
2025-04-05 03:53:44 -0500

    prototest

--- can_interface/can_backend.py
+++ can_interface/can_backend.py
@@ -19,7 +19,7 @@ idd = 0
 def create_sensor_data():
     sensor_data = template_pb2.SensorData()
     sensor_data.time = int(time.time_ns() // 1_000_000)
-    # sensor_data.packet_id = 1
+    sensor_data.packet_id = int(1)
     return sensor_data.SerializeToString()
 
 def publish_message(data):  
