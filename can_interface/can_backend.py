4380ab1392af9d1390a3823eaa89766e85b001f3
Anish-Y <55471849+Anish-Y@users.noreply.github.com>
2025-04-05 03:37:19 -0500

    prototest

--- can_interface/can_backend.py
+++ can_interface/can_backend.py
@@ -19,7 +19,7 @@ bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=1000000)
 
 def create_sensor_data(id):
     sensor_data = template_pb2.SensorData()
-    sensor_data.time = int(time.time() * 1000)
+    sensor_data.time = time.time_ns() // 1_000_000
     sensor_data.packet_id = id
     
 
