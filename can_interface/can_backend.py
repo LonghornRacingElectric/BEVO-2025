9e83c2d0208b87053ff972555f53b647011e1d97
Anish-Y <55471849+Anish-Y@users.noreply.github.com>
2025-04-05 03:20:42 -0500

    prototest

--- can_interface/can_backend.py
+++ can_interface/can_backend.py
@@ -10,8 +10,8 @@ import template_pb2
 import paho.mqtt.client as mqtt
 
 MQTT_BROKER = "192.168.1.109"
-MQTT_PORT = 8080
-MQTT_TOPIC = "data/"
+MQTT_PORT = 1883
+MQTT_TOPIC = "data"
 
 
 bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=1000000)
