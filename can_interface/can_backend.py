af1a36adbdaba9bf5615322c612090b5e51e71bb
Anish-Y <55471849+Anish-Y@users.noreply.github.com>
2025-04-06 19:17:48 -0500

    pls

--- can_interface/can_backend.py
+++ can_interface/can_backend.py
@@ -56,6 +56,7 @@ async def send_message(websocket):
             json_data = json.dumps(data)
             message_to_send = json_data
             try:
+                print("ttied")
                 await websocket.send(message_to_send)
                 # print(f"Sent message: {message_to_send}")
                 await asyncio.sleep(0.01)
