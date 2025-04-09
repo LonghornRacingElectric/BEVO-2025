65a7e24ea9c23720903f8c9f8432e9992edc7346
Anish-Y <55471849+Anish-Y@users.noreply.github.com>
2025-04-09 01:40:27 -0500

    pls2

--- can_interface/can_backend.py
+++ can_interface/can_backend.py
@@ -44,7 +44,7 @@ async def send_message(websocket):
                     can_buffer.append(data)
 
                     now = time.time()
-                    if now - last_tick >= 003.0:
+                    if now - last_tick >= 0.003:
                         p_id = int(os.getenv("p_id"))
                         proto.publish_msg(
                             mqtt_client=client, can_buffer=can_buffer, packet_id=p_id
