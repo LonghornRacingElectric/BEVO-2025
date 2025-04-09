d12a654e707752ecd33f0ac52767dfc492132d75
Anish-Y <55471849+Anish-Y@users.noreply.github.com>
2025-04-09 01:11:18 -0500

    pls2

--- can_interface/can_backend.py
+++ can_interface/can_backend.py
@@ -24,73 +24,73 @@ print(res.json()['last_packet'])
 os.environ["p_id"] = str(res.json()['last_packet'])
 
 
-async def send_message(websocket):
-    last_tick = time.time()
-    bus = can.interface.Bus(bustype="socketcan", channel="can0", bitrate=1000000)
-    last_can_time = 0
-    can_buffer = []
-    try:
-        while True:
-            try:
-                msg = bus.recv(timeout=0.01)
-                if(msg and (last_can_time - msg.timestamp < 0.1)):
-                    last_can_time = msg.timestamp
-                    data = {
-                        "id": msg.arbitration_id,
-                        "timestamp": msg.timestamp,
-                        "data": list(msg.data),
-                    }
-                    print(data)
-                    can_buffer.append(data)
+# async def send_message(websocket):
+#     last_tick = time.time()
+#     bus = can.interface.Bus(bustype="socketcan", channel="can0", bitrate=1000000)
+#     last_can_time = 0
+#     can_buffer = []
+#     try:
+#         while True:
+#             try:
+#                 msg = bus.recv(timeout=0.01)
+#                 if(msg and (last_can_time - msg.timestamp < 0.1)):
+#                     last_can_time = msg.timestamp
+#                     data = {
+#                         "id": msg.arbitration_id,
+#                         "timestamp": msg.timestamp,
+#                         "data": list(msg.data),
+#                     }
+#                     print(data)
+#                     can_buffer.append(data)
 
-                    now = time.time()
-                    if now - last_tick >= 003.0:
-                    #     p_id = int(os.getenv("p_id"))
-                    #     # proto.publish_msg(
-                    #     #     mqtt_client=client, can_buffer=can_buffer, packet_id=p_id
-                    #     # )
-                    #     os.environ["p_id"] = str(p_id + 1)
-                        can_buffer.clear()
-                        last_tick = now
+#                     now = time.time()
+#                     if now - last_tick >= 003.0:
+#                     #     p_id = int(os.getenv("p_id"))
+#                     #     # proto.publish_msg(
+#                     #     #     mqtt_client=client, can_buffer=can_buffer, packet_id=p_id
+#                     #     # )
+#                     #     os.environ["p_id"] = str(p_id + 1)
+#                         can_buffer.clear()
+#                         last_tick = now
 
-                message_to_send = json.dumps(data)
-                await websocket.send(message_to_send)
-                await asyncio.sleep(0.00018)
-            except Exception as e:
-                print(e)
-    except KeyboardInterrupt:
-        print("Stopping.")
-    finally:
-        bus.shutdown()
-
-
-async def handler(websocket):
-    print("client connected")
-    send_task = asyncio.create_task(send_message(websocket))
-
-    try:
-        while True:
-            try:
-                message = await websocket.recv()
-            except ConnectionClosedOK:
-                print("ConnectionClosedOK")
-                break
-            print(message)
-    except KeyboardInterrupt:
-        print("\n")
-
-    send_task.cancel()  # stop sending task
+#                 message_to_send = json.dumps(data)
+#                 await websocket.send(message_to_send)
+#                 await asyncio.sleep(0.00018)
+#             except Exception as e:
+#                 print(e)
+#     except KeyboardInterrupt:
+#         print("Stopping.")
+#     finally:
+#         bus.shutdown()
 
 
-async def main():
-    try:
-        while True:
-            print("Websocket server on localhost:8001")
-            async with serve(handler, "", 8001):
-                await asyncio.get_running_loop().create_future()
-    except asyncio.exceptions.CancelledError:
-        print("\nProgram interrupted. Exiting...")
+# async def handler(websocket):
+#     print("client connected")
+#     send_task = asyncio.create_task(send_message(websocket))
+
+#     try:
+#         while True:
+#             try:
+#                 message = await websocket.recv()
+#             except ConnectionClosedOK:
+#                 print("ConnectionClosedOK")
+#                 break
+#             print(message)
+#     except KeyboardInterrupt:
+#         print("\n")
+
+#     send_task.cancel()  # stop sending task
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+# async def main():
+#     try:
+#         while True:
+#             print("Websocket server on localhost:8001")
+#             async with serve(handler, "", 8001):
+#                 await asyncio.get_running_loop().create_future()
+#     except asyncio.exceptions.CancelledError:
+#         print("\nProgram interrupted. Exiting...")
+
+
+# if __name__ == "__main__":
+#     asyncio.run(main())
