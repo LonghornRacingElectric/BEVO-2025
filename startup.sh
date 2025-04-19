c5236a5e8108b727d19092596b121b5916fd6797
Anish-Y <55471849+Anish-Y@users.noreply.github.com>
2025-04-19 11:30:39 -0500

    uveill

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ TARGET="google.com"
 cd ~/Documents/BEVO-2025/
 source ./.venv/bin/activate
 
-cvlc BlankBevo.MP4 --fullscreen --play-and-exit
+vlc BlankBevo.MP4
 
 python cell_interface/cell_tools.py
 
