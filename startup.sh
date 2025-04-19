51c3817711f32e48843c4204a298b22fea3c7914
Anish-Y <55471849+Anish-Y@users.noreply.github.com>
2025-04-19 09:22:06 -0500

    unveil

--- startup.sh
+++ startup.sh
@@ -4,3 +4,4 @@ source ./.venv/bin/activate
 python cell_interface/cell_tools.py
 sudo ip link set can0 up type can bitrate 1000000
 python can_interface/scripts/can_backend.py
+mpg123 -q --loop -1 Texas Longhorns Fight Song
