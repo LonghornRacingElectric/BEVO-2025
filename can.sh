b51405330cbfe5b200fdb44b6279ed214bea7894
Anish-Y <55471849+Anish-Y@users.noreply.github.com>
2025-04-19 11:18:27 -0500

    uveill

--- can.sh
+++ can.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
 
-MAX_RETRIES=200
-RETRY_DELAY=2
-TARGET="google.com"
-
 cd ~/Documents/BEVO-2025/
 source ./.venv/bin/activate
-
-cvlc BlankBevo.MP4 --fullscreen --play-and-exit
-
-python cell_interface/cell_tools.py
-
-echo "Waiting for internet (DNS + Ping to $TARGET)..."
-
-for ((i=1; i<=MAX_RETRIES; i++)); do
-    if getent hosts "$TARGET" > /dev/null && ping -c 1 -W 1 "$TARGET" > /dev/null; then
-        echo "Internet is up!"
-        exit 0
-    fi
-    echo "[$i/$MAX_RETRIES] Still waiting..."
-    sleep $RETRY_DELAY
-done
-
-echo "Failed to connect after $((MAX_RETRIES * RETRY_DELAY)) seconds."
-exit 1
+cd ~/Documents/BEVO-2025/can_interface/scripts/
+python can_backend.py
---
+++ frontend.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ~/Documents/BEVO-2025/
+source ./.venv/bin/activate
+cd ~/Documents/BEVO-2025/frontend
+npm start
