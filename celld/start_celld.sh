a673973f6f12e7df90144177bd80d2eab8a24c08
Anish-Y <55471849+Anish-Y@users.noreply.github.com>
2025-06-17 20:13:47 -0500

    chicken jockey

---
+++ celld/start_celld.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate virtual environment
+source /home/lhre/Documents/BEVO-2025/.venv/bin/activate
+
+# Optional: add delay for system to settle
+sleep 2
+
+# Run Python script with argument
+python /home/lhre/Documents/BEVO-2025/celld/celld.py on
