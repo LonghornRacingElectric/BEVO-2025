cdd956be434c5b8724cd052cc2bcc8fcccbb7fc7
Anish-Y <55471849+Anish-Y@users.noreply.github.com>
2025-06-17 20:34:56 -0500

    celld.sh

--- celld/celld.sh
+++ celld/celld.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-sudo ip link set can0 up type can bitrate 1000000
+
+# Check if can0 interface is already up
+if ! ip link show can0 | grep -q "UP"; then
+    echo "Setting up CAN interface..."
+    sudo ip link set can0 up type can bitrate 1000000
+else
+    echo "CAN interface can0 is already up"
+fi
 
 # Activate virtual environment
 . /home/lhre/Documents/BEVO-2025/.venv/bin/activate
