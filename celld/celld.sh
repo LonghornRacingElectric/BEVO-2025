0713d4d40fe2fb6c4811e8e07a5a173946a01a5e
Anish-Y <55471849+Anish-Y@users.noreply.github.com>
2025-06-17 20:33:44 -0500

    celld.sh

--- celld/celld.sh
+++ celld/celld.sh
@@ -2,7 +2,7 @@
 sudo ip link set can0 up type can bitrate 1000000
 
 # Activate virtual environment
-source /home/lhre/Documents/BEVO-2025/.venv/bin/activate
+. /home/lhre/Documents/BEVO-2025/.venv/bin/activate
 
 # Run Python script with argument
 python /home/lhre/Documents/BEVO-2025/celld/celld.py on
